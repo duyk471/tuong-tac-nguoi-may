869ff7400a8635d1ff634db2156fc049f97fe01e
Khanh Duy <duykhanh471@protonmail.com>
2026-02-02 21:41:14 +0700

    Cập nhật

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 - [Main Quest 1](/main-quest-1/index.html)
 - [Main Quest 2](/main-quest-2/index.html)
-- [Main Quest 3](/main-quest-3/index.html) 
+- [Main Quest 3](/main-quest-3/index.html) - Xem [Spreadsheet trình bày](https://docs.google.com/spreadsheets/d/1JNJ4rQJFizZ3BPSjd1LoX6IXL1fe-O-MupeXCLSyxOs/edit?usp=sharing)
--- main-quest-3/index.js
+++ main-quest-3/index.js
@@ -1,6 +1,6 @@
 function toggleDevice() {
     const container = document.getElementById('app-container');
-    const btn = document.querySelector('.toggle-btn');
+    const btn = document.getElementById('device-toggle');
 
     if (container.classList.contains('mobile-view')) {
         container.classList.remove('mobile-view');
@@ -8,5 +8,22 @@ function toggleDevice() {
     } else {
         container.classList.add('mobile-view');
         btn.innerText = "Switch to Desktop View";
+    }
+}
+
+function toggleMenu() {
+    document.getElementById("dropdown-menu").classList.toggle("show");
+}
+
+// Close the menu if user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.user-avatar')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
 }
